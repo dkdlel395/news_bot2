2fb1dc74353af4826c9c47b40afefbb6fed50d8b
박상욱 <74848389+dkdlel395@users.noreply.github.com>
2024-01-26 15:08:32 +0900

    docs: get post 코드정리

--- main.py
+++ main.py
@@ -1,59 +1,69 @@
 import httpx
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
-from typing import Optional
 
 app = FastAPI()
 
-class Item(BaseModel):
-    price: str
-    cnt: str
-    @app.post("/items_test/")
-    async def items_test(item_price: price, item_cnt: cnt):
-        dict_price = dict(price)
-        return {item_price, item_cnt}
+# 2. 기본형
+@app.get("/")
+def main_re():
+    return 'hello youngs'
 
-
-
-
-# 루트 접속 시 main 으로 리다이렉트
+# 3. 루트 접속 시 main 으로 리다이렉트
 @app.get("/")
 def main_re():
     return RedirectResponse("http://127.0.0.1:8000/main/")
 
-# JSON 값을 리턴하는 API
+# 4. JSON 값을 리턴하는 API
 @app.get("/items/", response_class=JSONResponse)
 async def read_items():
     return {"item_id": "Foo"}
 
-# post 실험을 위한 라우터
-# @app.post("/cal_items/")#, response_class=JSONResponse)
-# async def cal_items(item_price: price, item_cnt: cnt):
-#     return {"item_price": item_price, "item_cnt": item_cnt}#, "total_price": item_price * item_cnt}
 
-# JSON 값을 리턴하는 API 값 call 함수
+# 5-함수 : JSON 값을 리턴하는 API 값 call 함수
 async def call_main():
     async with httpx.AsyncClient() as client:
         response = await client.get('http://127.0.0.1:8000/items/')
         return response.json()
 
-
-# post 실험을 위한 함수
-# async def call_cal():
-#     async with httpx.AsyncClient() as client:
-#         response = await client.post('http://127.0.0.1:8000/cal_items/', json={'item_price': '5', "item_cnt": '6'})
-#         return response.json()
-
-# API Call 함수로 값 요청하기
+# 5-API : API Call 함수로 값 요청하기
 @app.get("/main/")
 async def main_page(request: Request):
     client_host = request.client.host
     response_data = await call_main()
-    # response_cal = await call_cal()
     return {"client_host": client_host, "response_data": response_data} #, "response_cal": response_cal}
 
-@app.post('/ii/')
-async def create_item(item: Item):
-    return item
+# 6. get URL
+@app.get("/items/line/{item_id}")
+async def get_line(item_id: int):
+    return item_id
+
+# 7-BaseModel : 값 정리역할
+class Item(BaseModel):
+    price: int
+    cnt: int
+    name: str
+
+# 7-response : post test 받은 값을 리턴
+@app.post("/items_test/")
+async def items_test(item: Item):
+    # dict_price = {"item_price": item.price, "item_cnt": item.cnt}
+    return item.price * item.cnt, item.name
+
+
+# 7-requestpost : test json 값과 함께 post 요청
+@app.get('/ii/')
+async def read_item():
+    # POST 요청을 생성하여 /items_test 엔드포인트에 데이터 전송
+    async with httpx.AsyncClient() as client:
+        item_data = {"price": 10, "cnt": 5, "name":"youngs"}  # 원하는 데이터 설정
+        try:
+            response = await client.post("http://127.0.0.1:8000/items_test/", json=item_data)
+            response.raise_for_status()  # HTTP 오류가 있는 경우 예외 발생
+            return {"item_from_post": response.json()}
+        except httpx.HTTPError as e:
+            return {"error": f"HTTP error occurred: {str(e)}"}
+        except Exception as e:
+            return {"error": f"An error occurred: {str(e)}"}
